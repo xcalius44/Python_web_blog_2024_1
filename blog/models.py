214caa6f7162a508ade11851c2eb33fd3e6b148f
Artem <xcalius25@gmail.com>
2024-10-10 20:17:34 +0300

    UPDATE

--- blog/models.py
+++ blog/models.py
@@ -1,32 +1,57 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> models.QuerySet:
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
-        PUBLISHED = 'PB','Pablished'
-    
-    titel = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
         max_length=2,
         choices=Status,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()
+    published = PublishedManager()
     
     class Meta:
         ordering = ['-publish']
         indexes = [
-            models.Index(fields=['-publihs'])
+            models.Index(fields=['-publish']),
         ]
-    
+
     def __str__(self) -> str:
-        return self.titel
+        return self.title
     
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail", 
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
     
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView
 
-# Create your views here.
+from .forms import EmailPostForm
+from .models import Post
+
+
+class PostListView(ListView):
+
+    queryset=Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts': posts}
+    )
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
+    
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post': post}
+    )
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+
+
